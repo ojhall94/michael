4d3df906e9471e2d9238fd9f6d18452b2f86e829
Oliver <oliver.hall@esa.int>
2022-10-10 13:02:34 +0200

    updated decoder

--- michael/utils.py
+++ michael/utils.py
@@ -17,40 +17,24 @@ def _safety(janet):
 
 def _decode(flag):
     """
-    1 - SLS-obtained value
-    2 - SW-obtained value
-    4 - CACF-obtained value
-    8 - GP-obtained value
-    16 - No ACF measured
-    32 - ACF does not match 'best' period within 2 sigma
-    64 - ACF indicates that 'best' period is a potential harmonic
-    128 - Only two out of three estimates agreed
-    256 - No robust matches, CACF assumed best
-    512 - One or more sectors disagree strongly across all estimates
-    1024 - The result disagrees with a prior value.
-    2048 - The result is an integer multiple of the prior value (likely harmonic).
+    1 - No robust matches, CACF assumed best
+    2 - No robust matches, SW assumed best
+    4 - Only two out of three estimates agreed
+    8 - One or more sectors disagree strongly across all estimates
     """
 
     STRINGS = {
-        1 : "1: Best rotation is from the Simple Lomb Scargle (SLS) method.",
-        2 : "2: Best rotation is from the Simple Wavelet (SW) method.",
-        4 : "4: Best rotation is from the Composite Autocorrelation Function (CACF) method.",
-        8 : "8: Best rotation is from the Gaussian Process (GP) method.",
-        16 : "16: No ACF period could be reliably measured (indicating low power or long periods).",
-        32 : "32: The ACF period does not match the 'best' period within 2 sigma.",
-        64 : "64: The ACF period is potentially a harmonic of the 'best' period (or vice versa!)",
-        128 : "128: Only 2 of the 3 estimates of rotation agreed with one another " +
-             "to within 1 sigma.",
-        256 : "256: None of the 3 estimates agreed with one another to within 1 "+
+        1 : "1: None of the 3 estimates agreed with one another to within 1 "+
              "sigma. The CACF estimate is assumed to be the best in this case, "+
              "if it is available.",
-        512: "512: One or more sectors disagrees strongly across all estimates with " +
+        2 : "1: None of the 3 estimates agreed with one another to within 1 "+
+              "sigma. The SW estimate is assumed to be the best in this case, "+
+              "if it is available.",
+        4 : "128: Only 2 of the 3 estimates of rotation agreed with one another " +
+             "to within 1 sigma.",
+        8 :  "8: One or more sectors disagrees strongly across all estimates with " +
             "the others. This may indicate signal from a background star present " +
             "in those sectors.",
-        1024: "1024: The prior value is in disagreement with the 'best' period",
-        2048: "2048: The prior value is an integer multiple of the 'best' period. " +
-            "This may indicate that the 'best' period is a harmonic of the true "+
-            "period."
     }
 
     val = np.copy(flag)
