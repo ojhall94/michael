3b155edd371f3e54ed39d8371ed8e9cae4ba2721
Oliver <oliver.hall@esa.int>
2022-10-19 15:56:07 +0200

    pytesting going well sof ar

---
+++ tests/test_validate.py
@@ -0,0 +1,39 @@
+import sys
+import pytest
+sys.path.append('../')
+
+import numpy as np
+from numpy.testing import assert_almost_equal, assert_array_equal
+from michael import janet
+import astropy.units as u
+
+from michael.validate import validate_SLS
+from michael.validate import validate_SW
+from michael.validate import validate_CACF
+from michael.validate import validate_ACF
+from michael.validate import validate_best
+from michael.validate import validate_sectors
+from michael.validate import validate_p2p
+
+
+def test_validate_SLS():
+    return 0
+    
+def test_validate_SW():
+    return 0
+    
+def test_validate_CACF():
+    return 0
+    
+def test_validate_ACF():
+    return 0
+    
+def test_validate_best():
+    return 0
+    
+def test_validate_sectors():
+    return 0
+    
+def test_validate_p2p():
+    return 0
+    
