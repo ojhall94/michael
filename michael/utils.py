2f2a4c8cf99790d1e3784e29c4e0e8402c6e0d97
Oliver <oliver.hall@esa.int>
2022-10-21 17:01:38 +0200

    minor bug fix

--- michael/utils.py
+++ michael/utils.py
@@ -28,7 +28,7 @@ def _decode(flag):
         1 : "1: None of the 3 estimates agreed with one another to within 1 "+
              "sigma. The estimate with highest SNR is assumed to be the best in"+
              " this case.",
-        4 : "4: Only 2 of the 3 estimates of rotation agreed with one another " +
+        4 : "4: Only 3 or fewer of the 4 estimates of rotation agreed with one another " +
              "to within 1 sigma.",
         8 :  "8: One or more sectors disagrees strongly across all estimates with " +
             "the others. This may indicate signal from a background star present " +
